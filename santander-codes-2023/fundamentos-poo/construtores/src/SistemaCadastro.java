b38843b698b5b07540fe8b8509cbcd8f73e1ec41
devwander <josewanderson173@gmail.com>
2023-09-27 22:28:30 -0300

    Santander Bootcamp 2023 - Backend Java / Fundamentos da POO com Java - Construtores

---
+++ santander-codes-2023/fundamentos-poo/construtores/src/SistemaCadastro.java
@@ -0,0 +1,7 @@
+public class SistemaCadastro {
+	public static void main(String[] args) {
+		Pessoa marcos = new Pessoa("06724506716","MARCOS SILVA");
+
+    System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+	}
+}
