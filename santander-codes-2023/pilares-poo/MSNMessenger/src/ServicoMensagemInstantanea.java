44e37ee28bd5daa6d8081230b08f1415a3bd13ff
devwander <josewanderson173@gmail.com>
2023-09-29 14:43:55 -0300

    Santander Bootcamp 2023 - Backend Java / Pilates POO em Java - Abstração

--- santander-codes-2023/pilares-poo/MSNMessenger/src/ServicoMensagemInstantanea.java
+++ santander-codes-2023/pilares-poo/MSNMessenger/src/ServicoMensagemInstantanea.java
@@ -1,14 +1,6 @@
-public class ServicoMensagemInstantanea {
-	public void enviarMensagem() {
-		//primeiro confirmar se esta conectado a internet
-		validarConectadoInternet();
-		System.out.println("Enviando mensagem");
-		//depois de enviada, salva o histórico da mensagem
-		salvarHistoricoMensagem();
-	}
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem");
-	}
+public abstract class ServicoMensagemInstantanea {
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();	
 	
 	//métodos privadas, visíveis somente na classe
 	private void validarConectadoInternet() {
