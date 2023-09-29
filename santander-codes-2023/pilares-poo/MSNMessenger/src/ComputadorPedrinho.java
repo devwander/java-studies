9a61cea3e8b91ddda04a8dd40d493729faa3d990
devwander <josewanderson173@gmail.com>
2023-09-29 14:40:16 -0300

    Santander Bootcamp 2023 - Backend Java / Pilates POO em Java - Herança

--- santander-codes-2023/pilares-poo/MSNMessenger/src/ComputadorPedrinho.java
+++ santander-codes-2023/pilares-poo/MSNMessenger/src/ComputadorPedrinho.java
@@ -1,9 +1,17 @@
 public class ComputadorPedrinho {
 	public static void main(String[] args) {
-		MSNMessenger msn = new MSNMessenger();
-
-		msn.enviarMensagem();
 		
+		MSNMessenger msn = new MSNMessenger();
+		msn.enviarMensagem();
 		msn.receberMensagem();
+		
+		FacebookMessenger fbm = new FacebookMessenger();
+		fbm.enviarMensagem();
+		fbm.receberMensagem();
+		
+		Telegram tlg = new Telegram();
+		tlg.enviarMensagem();
+		tlg.receberMensagem();
+		
 	}
 }
